a4008bf436a6a73bf1f3e44b77103adc222029f2
nanduholkar <nanduholkar7@gmail.com>
2026-05-09 23:31:52 +0530

    feat: initialize React project setup with Vite, routing structure, and base configuration files

---
+++ reactrouter/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import "./index.css"
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-green-500'>React router dom</h1>
+    </>
+  )
+}
+
+export default App
